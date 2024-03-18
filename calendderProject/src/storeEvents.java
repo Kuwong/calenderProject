5bc5b69a385e8eb52fca9c225d6bc27136536afd
Kuwong <57968699+Kuwong@users.noreply.github.com>
2024-03-17 22:13:23 -0400

    Update storeEvents.java

--- calendderProject/src/storeEvents.java
+++ calendderProject/src/storeEvents.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-//import java.io.File;
 import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-
+import java.io.File;
 public class storeEvents{
 	
 	// Filename for the file that events are stored to.
 	public static final String FILENAME = "events.txt";
-	
+	public static final String FILE_NOT_FOUND = "Event file could not be found.";
 	/**
 	 * Checks if the file that the events are stored in exists yet.
 	 * 
@@ -15,17 +15,26 @@ public class storeEvents{
 	 * @return true if the file already exists/false if it does not
 	 */
 	public static boolean fileExists(String path) {
-		return false;
+	    File file = new File(path);
+	    return file.exists();
 	}
 	
 	/**
 	 * Updates the event save file by overwriting the file with the contents of the ArrayList
 	 * 
-	 * @param out
+	 * 
 	 * @param events
 	 */
-	public static void updateFile(PrintWriter out, ArrayList<String> events) {
-		
+	public static void updateFile(ArrayList<String> events) {
+	    try {
+	        PrintWriter edit = new PrintWriter(new File(FILENAME));
+	        for (String event : events) {
+	            edit.println(event);
+	        }
+	        edit.close(); 
+	    } catch (FileNotFoundException e) {
+	        System.out.println(FILE_NOT_FOUND);
+	    }
 	}
 	
 	/**
@@ -35,7 +44,18 @@ public class storeEvents{
 	 * @return ArrayList to be used by the createEvents class.
 	 */
 	public static ArrayList<String> readFile(Scanner input){
-		return null;
+	    ArrayList<String> events = new ArrayList<>();
+	    try {
+	        File file = new File(FILENAME);
+	        Scanner scanner = new Scanner(file);
+	        while (scanner.hasNextLine()) {
+	            String event = scanner.nextLine();
+	            events.add(event);
+	        }
+	        scanner.close();
+	    } catch (FileNotFoundException e) {
+	        System.out.println(FILE_NOT_FOUND); 
+	    }
+	    return events;
 	}
-	
 }
